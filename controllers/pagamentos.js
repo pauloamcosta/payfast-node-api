694b91313d3bee9c582597ac06b6c70bf9fd0f77
r_pauloandre <r_pauloandre@jfrn.jus.br>
2018-10-24 15:26:58 -0300

    creating route for payment and setting status and date

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -2,5 +2,13 @@ module.exports = function (app) {
     app.get('/pagamentos', function (req, res) {
         console.log('Recebida requisição de teste na porta 3000.')
         res.send('OK.')
-    })
+    });
+
+    app.post('/pagamentos/pagamento', function(req, res){
+        var pagamento = req.body;
+        console.log('processando uma requisição de um novo pagamento');
+        pagamento.status = 'Criado';
+        pagamento.data = new Date;
+        res.send(pagamento)
+    });
 }
